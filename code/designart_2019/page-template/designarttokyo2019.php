8ef03ab448740ceb75cc93a7d0d307b7a9b5720e
Leefee <toujo.aja@gmail.com>
2019-10-16 22:25:13 +0700

    update

--- code/designart_2019/page-template/designarttokyo2019.php
+++ code/designart_2019/page-template/designarttokyo2019.php
@@ -1,43 +1,42 @@
 <?php /* Template Name: DesignArt2019*/ ?>
 <?php
-get_header('top2');
+get_header( 'top2' );
 
-$post           = get_page_by_title('Designart 2019');
+$post           = get_page_by_title( 'Designart 2019' );
 $post_id        = $post->ID;
 $language       = get_key_languagle();
-$prefix_varible = get_prefix_languagle($language, "_");
-$description    = get_field($prefix_varible . 'description', $post_id, FALSE);
-$description    = str_replace('{%URL_STATICS%}', URL_STATICS, $description);
+$prefix_varible = get_prefix_languagle( $language, "_" );
+$description    = get_field( $prefix_varible . 'description', $post_id, false );
+$description    = str_replace( '{%URL_STATICS%}', URL_STATICS, $description );
 
-$html_share  = get_html_share(FALSE);
-$description = str_replace('{%HTML_SHARE%}', $html_share, $description);
+$html_share  = get_html_share( false );
+$description = str_replace( '{%HTML_SHARE%}', $html_share, $description );
 
-$description = str_replace('{%HOME_URL%}', home_url(), $description);
+$description = str_replace( '{%HOME_URL%}', home_url(), $description );
 
 $html_info   = get_html_information_post();
-$description = str_replace('{%INFORMATION_POST%}', $html_info, $description);
+$description = str_replace( '{%INFORMATION_POST%}', $html_info, $description );
 
 //    back history
-$html_back   = back_page_history(FALSE);
-$description = str_replace('{%BACK_HISTORY%}', $html_back, $description);
+$html_back   = back_page_history( false );
+$description = str_replace( '{%BACK_HISTORY%}', $html_back, $description );
 
 //    back history
-$html_countdown = getHtmlCountdown($post_id);
-$description    = str_replace('{%COUNT_DOWN%}', $html_countdown, $description);
+$html_countdown = getHtmlCountdown( $post_id );
+$description    = str_replace( '{%COUNT_DOWN%}', $html_countdown, $description );
 
-if (FALSE === ($json = get_transient('special_query_fb')))
-{
+if ( false === ( $json = get_transient( 'special_query_fb' ) ) ) {
 
 	$accessToken = 'EAAGeTfHqa2sBAGT5cdF3PZAqZAibKrtFBzHTpHMRSZBK8x7pgsWkFC5Lt8m8xUyFIdZBM1qjiC4ZCZC4giAexGs5dD3AkHukA8dEU3IV5CQExSQFQ7gGMtFXVwx5M66z7IUEZAOvgZBF9XVkUJt3pTVP2MhAMWemhAWpsldIdEZCVSgZDZD'; // designart アクセストークン(User Token)
 	$search      = 'designart.jp/feed';
 	$url         = 'https://graph.facebook.com/v4.0/' . $search . '?access_token=' . $accessToken;
-	$jsonData    = file_get_contents($url);
+	$jsonData    = file_get_contents( $url );
 
 	// jsonデータの整形・出力
-	$json = json_decode($jsonData, TRUE);
+	$json = json_decode( $jsonData, true );
 //    $str = json_encode($json['data']);
 	// It wasn't there, so regenerate the data and save the transient
-	set_transient('special_query_fb', $json, 6000);
+	set_transient( 'special_query_fb', $json, 6000 );
 }
 
 $active  = 'active';
@@ -51,24 +50,23 @@ $format  = '<div class="js-slide u-bg-img-hero-center">
                                 </div>
                                 </div>';
 $content = '';
-foreach ($json['data'] as $value):
-	if (empty($value['message']))
-	{
+foreach ( $json['data'] as $value ):
+	if ( empty( $value['message'] ) ) {
 		continue;
 	}
 //	wp-content/themes/designart_2019/statics/images/top/banner2019.jpg
-	$time = date("Y年m月d日", strtotime($value['created_time']));
-	$str1 = trim(strtok($value['message'], "\n"));
-	$str1 = strlen($str1) > 25 ? mb_substr($str1, 0, 25) . "..." : $str1;
+	$time = date( "Y年m月d日", strtotime( $value['created_time'] ) );
+	$str1 = trim( strtok( $value['message'], "\n" ) );
+	$str1 = strlen( $str1 ) > 25 ? mb_substr( $str1, 0, 25 ) . "..." : $str1;
 //	$str1    = preg_replace( '/[^\w\s]+/u', '', strlen( $str1 ) > 70 ? substr( $str1, 0, 70 ) . "..." : $str1 );
-	$id = explode('_', $value['id']);
+	$id = explode( '_', $value['id'] );
 
-	$content .= sprintf($format, $active, $time, $str1, $id[1]);
+	$content .= sprintf( $format, $active, $time, $str1, $id[1] );
 
 	$active = '';
 endforeach;
 
-$description = str_replace('{%CAROUSEL_NEWS%}', $content, $description);
+$description = str_replace( '{%CAROUSEL_NEWS%}', $content, $description );
 
 /*$exhibitors = get_posts(array(
 	'post_type'      => 'exhibitor',
@@ -78,23 +76,22 @@ $description = str_replace('{%CAROUSEL_NEWS%}', $content, $description);
 	'order'          => 'DESC'
 //    'orderby'    => ['post_modified' => 'desc'],
 ));*/
-$events     = get_posts(array(
+$events = get_posts( array(
 	'post_type'      => 'event-party',
-	'posts_per_page' => 10,
+	'posts_per_page' => 5,
 	'order'          => 'DESC',
 	'orderby'        => 'post_date',
-
-));
+	'post_status'    => [ 'future', 'private' ],
+) );
 
 $exhibitor_list              = '';
 $exhibitor_thumbnail_list    = '';
 $exhibitor_list_mb           = '';
 $exhibitor_thumbnail_list_mb = '';
-foreach ($events as $event)
-{
+foreach ( $events as $event ) {
 	$exhibitor_id = $event->ID;
-	$gallery      = get_field('thumbnail', $exhibitor_id);
-	$time         = get_field('time_schedule', $exhibitor_id);
+	$gallery      = get_field( 'thumbnail', $exhibitor_id );
+	$time         = get_field( 'time_schedule', $exhibitor_id );
 
 
 	$thumbail_url = $gallery;
@@ -102,7 +99,7 @@ foreach ($events as $event)
 	{
 		$thumbail_url = take_value_array('url', $gallery[0]);
 	}*/
-	$exhibitor_title = get_field($prefix_varible . 'title', $exhibitor_id);
+	$exhibitor_title = get_field( $prefix_varible . 'title', $exhibitor_id );
 
 	$exhibitor_list           .= '<div class="exhibitor_mySlides desc-wrapp">
                                          <p class="text-16">' . $time . ':</p> 
@@ -129,10 +126,10 @@ foreach ($events as $event)
 }
 
 
-$description = str_replace('{%EXHIBITOR_LIST%}', $exhibitor_list, $description);
-$description = str_replace('{%EXHIBITOR_THUMBNAIL%}', $exhibitor_thumbnail_list, $description);
-$description = str_replace('{%EXHIBITOR_LIST_MB%}', $exhibitor_list_mb, $description);
-$description = str_replace('{%EXHIBITOR_THUMBNAIL_MB%}', $exhibitor_thumbnail_list_mb, $description);
+$description = str_replace( '{%EXHIBITOR_LIST%}', $exhibitor_list, $description );
+$description = str_replace( '{%EXHIBITOR_THUMBNAIL%}', $exhibitor_thumbnail_list, $description );
+$description = str_replace( '{%EXHIBITOR_LIST_MB%}', $exhibitor_list_mb, $description );
+$description = str_replace( '{%EXHIBITOR_THUMBNAIL_MB%}', $exhibitor_thumbnail_list_mb, $description );
 echo $description;
 
 //$prefix_varible_slider = get_prefix_languagle( $language, "-" );
@@ -142,7 +139,7 @@ echo $description;
 ?>
 
 
-<?php get_footer('top2'); ?>
+<?php get_footer( 'top2' ); ?>
 
 <script type="text/javascript">
     /* Add current menu
@@ -163,7 +160,6 @@ echo $description;
 
                 }
             );
-
 
 
             setTimeout(function () {
