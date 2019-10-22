bdd30a59ad7d5d291efcb1f49703a60facfe268f
Thang <phamdinhthang1412@gmail.com>
2019-10-22 22:57:21 +0700

    update l

--- code/designart_2019/page-template/archive-exhibitor.php
+++ code/designart_2019/page-template/archive-exhibitor.php
@@ -205,7 +205,9 @@ $cate = isset( $_GET['cate'] ) ? $_GET['cate'] : '';
 <script>
     jQuery(function ($) {
         $(document).ready(function () {
-            let menu = $('a[data-demo=item-1]').parents('li.menu-item');
+            let ahref = $('a[data-demo=item-1]');
+            console.log(ahref);
+            let menu = ahref.parents('li.menu-item');
             console.log(menu);
             menu.addClass('current-menu-item');
             $('.filter_category a.category').on('click', function (e) {
